c595d0415ba8a57108b9f57c8c357c01dd662237
Kevin Munger <kevinmunger@gmail.com>
2019-03-27 13:43:38 -0400

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# PRESS: Power Analysis
+
+![](C:/Users/kevin/Documents/GitHub/PRESS_power_analysis/img.jpg)
+
+This repository contains the materials for a PRESS skills workshop presented at Princeton University Rosario in March June, 2019. The materials here are all based on materials in the DeclareDesign website. 
+
+
+https://declaredesign.org/library/
+
+
+
---
+++ power_analysis_press.R
@@ -0,0 +1,39 @@
+
+
+
+library(DeclareDesign)
+library(tidyverse)
+
+
+design <-
+  declare_population(
+    N = 1150,
+    #range of age blocks
+    age = sample(-4:4, size = N, replace = TRUE), 
+    noise = rnorm(N)
+  ) +
+  declare_potential_outcomes(Y ~ main * Z + age_fx*age + het * age * Z + noise) +
+  declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0), interaction = het) +
+  declare_assignment(prob = 0.5) +
+  declare_reveal(Y, Z) +
+  declare_estimator(
+    Y ~ Z  + Z * age,
+    model = lm_robust,
+    term = c("Z", "Z:age"),
+    estimand = c("ATE", "interaction")
+  )
+
+designs <- redesign(design, het = c( .05, .07 , .09), main = c(.2), age_fx = c(.2))
+simulations <- simulate_design(designs, sims = 100)
+summary_df <-
+  simulations %>%
+  group_by(het, main, estimand_label) %>%
+  summarize(power = mean(p.value <= 0.05),
+            mean_estimate = mean(estimate))  
+
+
+ggplot(summary_df, aes(het, power, group = estimand_label, color = estimand_label)) +
+  geom_point() +
+  geom_line() +
+  geom_hline(yintercept = 0.8) +
+  theme_bw()
